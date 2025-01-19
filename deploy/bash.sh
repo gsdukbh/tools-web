b1a0d1ffd93602be4d0609dfc55a95de3922cce2
JiaWei Lee <gsdukbh@gmail.com>
2025-01-19 16:32:16 +0800

    🚀 chore(deploy/bash.sh): 使用 buildx 构建 Docker 镜像以提高构建效率 🔧 fix(build.gradle): 更新版本号至 0.0.9-releases 🎉 ci(.gitlab-ci.yml): 修复部署脚本的执行方式，确保正确运行 bash 脚本

--- deploy/bash.sh
+++ deploy/bash.sh
@@ -4,10 +4,10 @@ if docker compose ps | grep -q 'Up'; then
     echo "Tools is running, stop and restart"
     docker compose down
     docker rmi gsdukbh/tools-web
-    docker build -t $IMAGE_TAG -f Dockerfile ./
+    docker buildx build -t $IMAGE_TAG -f Dockerfile ./
     docker compose up -d
 else
     echo "Tools is not running, start"
-    docker build -t $IMAGE_TAG -f Dockerfile ./
+    docker buildx build -t $IMAGE_TAG -f Dockerfile ./
     docker compose up -d
 fi
