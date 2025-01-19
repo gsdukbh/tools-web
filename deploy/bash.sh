23aaadc1e899c0795e7a60ddfdbd18298163f4d0
JiaWei Lee <gsdukbh@gmail.com>
2025-01-19 15:40:19 +0800

    🐛 修复(deploy/bash.sh): 将 docker-compose 命令更新为 docker compose 以保持一致性和兼容性。

--- deploy/bash.sh
+++ deploy/bash.sh
@@ -1,6 +1,6 @@
 # bin/bash
 cd /tools
-docker-compose down
+docker compose down
 docker rmi gsdukbh/tools-web
 docker build -t $IMAGE_TAG -f Dockerfile ./
-docker-compose up -d
+docker compose up -d
