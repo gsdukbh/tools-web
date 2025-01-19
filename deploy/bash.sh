13fb035403e02ee7cac0bd221b34a69aa09bebbb
JiaWei Lee <gsdukbh@gmail.com>
2025-01-19 16:10:29 +0800

    🔧 chore(deploy): 更新docker-compose和bash脚本以支持工具的构建与部署

--- deploy/bash.sh
+++ deploy/bash.sh
@@ -1,11 +1,13 @@
 # bin/bash
 cd /tools
 if docker compose ps | grep -q 'Up'; then
+    echo "Tools is running, stop and restart"
     docker compose down
     docker rmi gsdukbh/tools-web
     docker build -t $IMAGE_TAG -f Dockerfile ./
     docker compose up -d
 else
+    echo "Tools is not running, start"
     docker build -t $IMAGE_TAG -f Dockerfile ./
     docker compose up -d
 fi
